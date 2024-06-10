213565acd43792be1b22979df24fb65345d9855f
Passpartoo <c9pellet+github@gmail.com>
2024-06-10 18:52:11 +0200

    Update EniCrypt.py

--- EniCrypt.py
+++ EniCrypt.py
@@ -4,4 +4,7 @@ from tkinter import ttk
 class EniCrypt(ttk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        #self.enicrypt_widgets()
+
+
         # Widgets de l'onglet EniCrypt
